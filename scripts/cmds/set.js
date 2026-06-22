d3baeb97dcb8a987067388e58d1b86576178d96c
taohidpro17 <fackidpro97@gmail.com>
2026-06-22 22:19:02 +0600

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -19,7 +19,7 @@ module.exports = {
 
   onStart: async function ({ args, event, api, usersData }) {
 
-    const ADMIN = ["61583129938292", "100081088184521"];
+    const ADMIN = ["61590776610353", "61590776610353"];
     if (!ADMIN.includes(event.senderID)) {
       return api.sendMessage(
         "age owner level e asho broo 🌬️",
