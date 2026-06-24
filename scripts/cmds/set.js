4461b4f5520c7622d22b46d0e39399c21fddf9cf
taohidpro17 <fackidpro97@gmail.com>
2026-06-24 09:21:13 +0600

    Update ADMIN IDs in set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -19,7 +19,7 @@ module.exports = {
 
   onStart: async function ({ args, event, api, usersData }) {
 
-    const ADMIN = ["61590918371596", "61590918371596"];
+    const ADMIN = ["61591515380799", "61591515380799"];
     if (!ADMIN.includes(event.senderID)) {
       return api.sendMessage(
         "age owner level e asho broo 🌬️",
